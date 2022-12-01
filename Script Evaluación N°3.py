aac87ae817b6ffb20e4893bd51127977499d4d1d
Magarooo <daniellabarcacamus@hotmail.com>
2022-12-01 18:29:43 +0000

    Subir Script de lista a Github

---
+++ Script Evaluación N°3.py
@@ -0,0 +1,14 @@
+## Script Evaluación N°3
+
+Alumnos = ["Daniell Abarca", "Matias Mella"]
+
+i=0
+    
+    
+print("Los alumnos que rendirán la evaluación 3 serán los siguientes: \n")
+
+i=0
+
+for i in range(i,len(Alumnos)):
+    print("Estudiante n°",i+1,":", Alumnos[i])
+print("Programa Finalizado") 
